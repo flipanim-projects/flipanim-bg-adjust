78736f7494b88dc4ea01bddacbcc7a0374f8a3f1
pr <pineapplerind.info@gmail.com>
2022-04-22 21:22:52 -0400

    clean

--- content.js
+++ content.js
@@ -1,23 +1,9 @@
+// fetch the colors stored from chrome's storage
 chrome.storage.sync.get(["mainColor", "bodyColor"], ({ mainColor, bodyColor }) => {
     document.querySelector('.main').style.backgroundColor = mainColor;
     document.body.style.backgroundColor = bodyColor
-    let browseIcons = document.querySelectorAll('.browse_item a>div>div')
+    let browseIcons = document.querySelectorAll('.browse_item a>div>div') // the icons shown on an anim thumbnail
     for (let i = 0; i < browseIcons.length; i++) {
-        browseIcons[i].style.backgroundColor = mainColor
+        browseIcons[i].style.backgroundColor = mainColor // set the background of those to the main background color
     }
 });
-
-
-/* for later
-
-function computeTextColor(bgColor) {
-    var color = (bgColor.value.charAt(0) === '#') ? bgColor.value.substring(1, 7) : bgColor.value;
-    var r = parseInt(color.substring(0, 2), 16), // hexToR
-        g = parseInt(color.substring(2, 4), 16), // hexToG
-        b = parseInt(color.substring(4, 6), 16); // hexToB
-    let final = (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186) ?
-        'black' : 'white';
-    return final
-} 
-
-*/
